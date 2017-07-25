13156a1c9a31104785eeaa0ed4757815d42aa7e3
Nathan Cox <nathan@flyingmonkey.co.nz>
2017-07-25 16:05:23 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+SilverStripe VersionedGridRow
+===================================
+
+This module extends GridField to add some styles and labels for showing the published state of versioned objects.
+
+![screenshot](./screenshot1.png)
+
+
+Maintainer Contacts
+-------------------
+Nathan Cox (<me@nathan.net.nz>)
+
+Requirements
+------------
+* SilverStripe 3.1+
+
+Documentation
+-------------
+[GitHub](https://github.com/nathancox/silverstripe-versionedgridrows/wiki)
+
+Installation Instructions
+-------------------------
+
+With composer:
+
+`composer require nathancox/versionedgridrows`
+
+
+Usage Overview
+--------------
+
+Configuration via `config.yml`:
+
+```yaml
+
+VersionedGridRow:
+  mode: 'component'			# can be "component", "config" or "always"
+  show_published: false		# set to true to show a flag on published items as well
+    
+```
+
+Options for the mode setting:
+
+*mode: 'component'*
+
+Only add row styles to GridFields with the VersionGridRows component added to them.  This is done like any other component:
+
+`
+$gridFieldConfig->addComponent(new VersionedGridRows('Title'));
+`
+The constructor argument ("Title") specifies the name of the column the "draft" or "modified" flags will be appended to.  Leave blank to not automatically append the flags.
+
+*mode: 'always'*
+
+Styles will be added to all GridFields that managed versioned objects.  If you want to add flags  you need to do it manually with something like this:
+
+`
+$summary_fields = array(
+	'VersionedTitle' => 'Title'
+);
+
+...
+
+public function VersionedTitle()
+{
+	return VersionedGridRows::get_cell_content($this, $this->Title);
+}
+
+`
+
+*mode: 'config'*
+
+Specify a list of classes that styles will be added to.
+
+`
+VersionedGridRow:
+  mode: 'config'
+  classes:
+  	- Page
+  	- StaffMember
+  	- HomepageSlide
+`
+
+You will have to add version status flags manually as shown above.
+
+
+Known Issues
+------------
+[Issue Tracker](https://github.com/nathancox/silverstripe-versionedgridrows/issues)
---
+++ code/VersionedGridRows.php
@@ -0,0 +1,71 @@
+<?php
+
+class VersionedGridColumn extends GridFieldDataColumns
+{
+
+	public $fieldToFlag = false;
+
+	public function __construct($fieldToFlag = false)
+	{
+		$this->fieldToFlag = $fieldToFlag;
+	}
+
+
+	public function getColumnContent($gridField, $record, $columnName)
+	{
+		if ($this->fieldToFlag && $columnName == $this->fieldToFlag) {
+			return VersionedGridRows::get_status_html($record);
+		}
+	}
+
+    /**
+     * Return the publication status of the record
+     * @param  DataObject $record The object this row represents.
+     * @return string         "draft", "modified" or "published"
+     */
+    public static function get_status($record)
+    {
+        $flag = false;
+        if ($record->hasExtension('Versioned')) {
+            $latestPublished = $record->latestPublished();
+
+            if ($latestPublished === null) {
+                $flag = 'draft';
+            } else {
+                $flag = ($latestPublished ? 'published' : 'modified');
+            }
+        }
+
+        return $flag;
+    }
+
+    /**
+     * Get the HTML for the "draft" or "modified" label
+     * @param  DataObject $record   The object this row represents.
+     * @return string
+     */
+    public static function get_status_html($record)
+    {
+        $output = '';
+        $flag = VersionedGridRows::get_status($record);
+        if ($flag && ($flag != 'published' || Config::inst()->get('VersionedGridRow', 'show_published') === true)) {
+            $output = ' <span class="state-marker '.$flag.'">'.ucfirst($flag).'</span>';
+        }
+
+        return $output;
+    }
+
+
+    /**
+     * Returns the given string with the appropriate marker appended, for use in GridField cells.
+     * @param  DataObject $record   The object this row represents.
+     * @param  string $text         The text content of the cell (eg $this->Title)
+     * @return DBField
+     */
+    public static function get_cell_content($record, $text)
+    {
+        return DBField::create_field('HTMLVarchar', $text . ' ' . self::get_status_html($record));
+    }
+
+
+}
